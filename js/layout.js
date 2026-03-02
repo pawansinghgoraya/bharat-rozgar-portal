4d0008a381b6208bae217b14421e6ef001187d6a
PAWAN SINGH GORAYA <pawansinghgoraya@gmail.com>
2026-03-02 19:10:43 +0530

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -11,3 +11,15 @@ fetch(basePath + "/components/footer.html")
   .then(data => {
     document.getElementById("footer-placeholder").innerHTML = data;
   });
+
+// Active link highlight
+setTimeout(() => {
+  const currentPath = window.location.pathname;
+  const navLinks = document.querySelectorAll(".main-nav a");
+
+  navLinks.forEach(link => {
+    if (currentPath.includes(link.getAttribute("href"))) {
+      link.classList.add("active-link");
+    }
+  });
+}, 300);
